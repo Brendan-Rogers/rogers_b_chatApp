940668627899c6fcbc45725af9585308425a8192
Brendan Rogers <brendanr11@gmail.com>
2019-03-27 13:23:00 -0400

    Made the messages send

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')();
 
 const port = process.env.PORT || 3000;
 
@@ -12,7 +11,24 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-http.listen(port, () =>{
+const server = app.listen(port, () => {
+	console.log(`app is running on port ${port}`);
+});
 
-    console.log(`app is running on port ${port}`)
+io.attach(server);
+
+io.on('connection', function(socket) {
+	console.log('a user has connected');
+	socket.emit('connected', { sID: `${socket.id}`, message: `new connection` });
+
+	// listen for incoming message from anyone connected to the app
+	socket.on('chat message', function(msg) {
+		console.log('message: ', msg, 'socket: ', socket.id);
+
+		io.emit('chat message', { id: `${socket.id}`, message: msg })
+	});
+
+	socket.on('disconnect', function() {
+		console.log('a user has disconnect');
+	});
 });
